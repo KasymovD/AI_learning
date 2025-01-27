113432c3cf5c3967c344505fa209fb63a27b0f51
KasymovD <ksmv982@gmail.com>
2025-01-26 23:51:30 -1200

    Update 2

---
+++ train.py
@@ -0,0 +1,16 @@
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from model import Net
+from utils import get_device
+
+def train(model, device, train_loader, optimizer, criterion, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()

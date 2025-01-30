af0902578d2d64940b166696b449053ad77e6892
KasymovD <ksmv982@gmail.com>
2025-01-30 15:39:20 +0800

    Update 3.0

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/dnn.hpp>
 #include <iostream>
+#include <vector>
+#include <filesystem>
 
 using namespace cv;
 using namespace cv::dnn;
 using namespace std;
+namespace fs = std::filesystem;
 
-int main() {
-    Mat inputImage = imread("image.png", IMREAD_GRAYSCALE);
-    if (inputImage.empty()) {
-        cerr << "Image loading error!" << endl;
-        return -1;
-    }
-
-    resize(inputImage, inputImage, Size(28, 28));
-    inputImage.convertTo(inputImage, CV_32F, 1.0 / 255.0);
-
+Net createCNN() {
     Net cnn;
 
     cnn.addLayerToPrev("input", "Input", LayerParams());
@@ -39,18 +33,54 @@ int main() {
 
     cnn.addLayerToPrev("softmax", "Softmax", LayerParams());
 
-    Mat blob = blobFromImage(inputImage);
+    return cnn;
+}
 
-    cnn.setInput(blob);
-    Mat output = cnn.forward();
+Mat preprocessImage(const string& imagePath) {
+    Mat inputImage = imread(imagePath, IMREAD_GRAYSCALE);
+    if (inputImage.empty()) {
+        throw runtime_error("Error loading image: " + imagePath);
+    }
 
-    Point classIdPoint;
-    double confidence;
-    minMaxLoc(output, nullptr, &confidence, nullptr, &classIdPoint);
-    int predictedClass = classIdPoint.x;
+    resize(inputImage, inputImage, Size(28, 28));
+    inputImage.convertTo(inputImage, CV_32F, 1.0 / 255.0);
+    return blobFromImage(inputImage);
+}
 
-    cout << "Predicted class: " << predictedClass << endl;
-    cout << "Confidence: " << confidence << endl;
+void classifyImages(Net& cnn, const vector<string>& imagePaths) {
+    for (const auto& imagePath : imagePaths) {
+        try {
+            Mat blob = preprocessImage(imagePath);
+            cnn.setInput(blob);
+            Mat output = cnn.forward();
+
+            Point classIdPoint;
+            double confidence;
+            minMaxLoc(output, nullptr, &confidence, nullptr, &classIdPoint);
+            int predictedClass = classIdPoint.x;
+
+            cout << "Image: " << imagePath << endl;
+            cout << "Predicted class: " << predictedClass << endl;
+            cout << "Confidence: " << confidence << endl << endl;
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <image_path1> [<image_path2> ...]" << endl;
+        return -1;
+    }
+
+    vector<string> imagePaths;
+    for (int i = 1; i < argc; ++i) {
+        imagePaths.push_back(argv[i]);
+    }
+
+    Net cnn = createCNN();
+    classifyImages(cnn, imagePaths);
 
     return 0;
 }

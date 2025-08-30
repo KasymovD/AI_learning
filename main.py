fbfeb9e821b2f148240b474eabfdde0610abb579
KasymovD <ksmv982@gmail.com>
2025-08-30 20:10:21 +0800

    update

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import torch
-import numpy as np
 
-scalar = torch.tensor(7) # Scalar value
+a = torch.tensor([[1., 2., 3.],
+                  [4., 5., 6.]], requires_grad=True)
 
-print(scalar.item())
-print(scalar.shape)
-print(scalar.ndim)
-print(scalar[0])
+b = torch.tensor([[7., 8.],
+                  [9., 10.],
+                  [11., 12.]], requires_grad=True)
 
-vector = torch.tensor([1, 2, 3])
+c = a @ b
+d = torch.sum(c)
 
-print(vector.shape)
-print(vector.ndim)
-print(vector[0])
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("d:", d)
 
-matrix = torch.tensor([[1,3],[3,1]])
+d.backward()
 
-print(matrix.shape)
-print(matrix.ndim)
-print(matrix[0])
+print("Grad a:", a.grad)
+print("Grad b:", b.grad)
 
-tensor = torch.tensor([
-    [[1, 2, 3],
-    [3, 6, 9],
-    [2, 4, 5],
-    [4, 2, 1]],
+x = torch.randn(3, 3)
+print("x:", x)
+print("x mean:", x.mean())
+print("x std:", x.std())
 
-    [[2, 3, 4],
-     [5, 2, 5],
-     [7, 6, 3],
-     [3, 4, 5]]
-    ])
+x_reshaped = x.view(1, 9)
+print("x reshaped:", x_reshaped)
 
-print(tensor.shape)
-print(tensor.ndim)
-print(tensor[0])
+y = torch.linspace(0, 1, steps=5)
+print("y:", y)
+
+z = torch.rand((2, 2))
+print("z:", z)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+t = torch.ones((3, 3), device=device)
+print("t:", t)
